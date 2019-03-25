e042da9c39969f18890df8e5c32a2d778eeb048f
ericrlin <eric@barnabasrobotics.com>
2019-03-25 10:40:22 -0700

    Add files via upload
    Showcase of single encoder tracking motor progress

---
+++ singleencodertest.ino
@@ -0,0 +1,24 @@
+int Motor1A=9;
+int Motor1B=10;
+int encoder=3;
+int count=0;
+
+void counter(){
+  count++;
+}
+
+void setup() {
+  pinMode(Motor1A,OUTPUT);
+  pinMode(Motor1B,OUTPUT);
+  pinMode(encoder,INPUT);
+  attachInterrupt(digitalPinToInterrupt(encoder),counter,RISING);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  analogWrite(Motor1A,150);
+  digitalWrite(Motor1B,LOW);
+  Serial.println(count);
+
+}
